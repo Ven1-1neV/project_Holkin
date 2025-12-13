7f0d5601af8e0cbbf6a7c2dbb831bd8ef04727a3
Ven1-1neV <danelhruchatik@gmail.com>
2025-12-13 11:56:13 +0300

    Update PZ_4_1.py

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,19 +1,29 @@
 #даны 2 целых числа A и B  а меньше б вывести в порядек возрастания 
 # все целые числа, расоложенные между а и б, включая A и B 
 # а также количество этих чисел
-a, b = int(input("введите число А")), int(input('введите число B > A'))
-z = 1
-x = a
-if a > b:
-    print('число А должно быть меньше числа b, пока')
-    end
+a, b = input("введите число А"), input('введите число B > A')
+while type(a) != int:
+    try:
+        a = int(a)
+    except ValueError:
+        print('неправильно ввели')
+        a = input('введите число')
+        
+while type(b) != int:
+    try:
+        b = int(b)
+    except ValueError:
+        print('неправильно ввели')
+        b = input('введите число')
+k = 1 
+z = b - a + 1
+x = a 
 print('числа по возрастанию;')
 print(a)
-while z != x:
-    z = b - a 
-    k = x
+while z != k:
     x += 1
+    k += 1
     print(x)
-print(b)
+
 N = b - a + 1
 print("всего чисел:", N)
